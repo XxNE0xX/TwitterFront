18768abb40c432daa33fda635c79daaaf7402a84
XxNE0xX <kiancoder1@gmail.com>
2021-01-26 15:37:47 +0330

    Homepage leftside completed.

--- src/Components/Javascripts/HomePage/Goal.js
+++ src/Components/Javascripts/HomePage/Goal.js
@@ -1,17 +1,33 @@
 import '../../Styles/HomePage/HomePageInformationPanel.css';
 import React from "react";
-import icon from "../../../Images/HomePage/Search.png";
+import searchIcon from "../../../Images/HomePage/Search.png";
+import twoMenIcon from "../../../Images/HomePage/TwoMen.png";
+import speechIcon from "../../../Images/HomePage/SpeechBubble.png";
 
 export default class HomePageInformationPanel extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            url: null,
+        };
     }
 
     render() {
+
+        let image;
+        if (this.props.iconType === "Search"){
+            image = searchIcon;
+        }
+        else if (this.props.iconType === "TwoMen"){
+            image = twoMenIcon;
+        }
+        else if (this.props.iconType === "Speech"){
+            image = speechIcon;
+        }
         return (
             <div className="Goal">
-                <div className="GoalImage" style={{backgroundImage: `url(${this.props.iconPath})`}}/>
+                <div className="GoalImage" style={{backgroundImage: `url(${image})`}} />
                 <div className="GoalText">{this.props.text}</div>
             </div>
         );
--- src/Components/Javascripts/HomePage/GoalsContainer.js
+++ src/Components/Javascripts/HomePage/GoalsContainer.js
@@ -11,9 +11,9 @@ export default class HomePageInformationPanel extends React.Component {
     render() {
         return (
             <div className="GoalsContainer">
-                <Goal iconPath={"../../../Images/HomePage/Search.png"} text={"Follow your interests."} />
-                <Goal iconPath={"../../../Images/HomePage/TwoMen.png"} text={"Hear what people are talking about."} />
-                <Goal iconPath={"../../../Images/HomePage/SpeechBubble.png"} text={"Join the conversation."} />
+                <Goal iconType={"Search"} text={"Follow your interests."} />
+                <Goal iconType={"TwoMen"} text={"Hear what people are talking about."} />
+                <Goal iconType={"Speech"} text={"Join the conversation."} />
             </div>
         );
     }
