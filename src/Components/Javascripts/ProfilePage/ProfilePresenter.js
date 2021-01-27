c43b8886b484c2b4b9574dcd8c9bb489e3089c93
XxNE0xX <kiancoder1@gmail.com>
2021-01-27 21:34:41 +0330

    Profile Page is added.

---
+++ src/Components/Javascripts/ProfilePage/ProfilePresenter.js
@@ -0,0 +1,111 @@
+import "../../Styles/ProfilePage/ProfilePresenter.css";
+
+import Tweet from "../Tweet/Tweet";
+import {Button} from "antd";
+import React from "react";
+
+export default class FeedTweetsContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        // const tweets = this.props.tweetIDs.reverse().map((tweetID)=>{
+        //     console.log(tweetID)
+        //     return <Tweet key={tweetID} tweetID={tweetID}/>
+        // }
+        // )
+        return (
+            <div className="ProfilePresenterContainer">
+                <img src={"../../../Images/HomePage/TwitterIconWhite.png"} className={"CoverPicture"}/>
+                <ButtonsBar isFollowed={true} profilePicturePath={""}/>
+                <UserInformationPanel UsersName={"Shalqam"} Username={"_Sh99"} Bio={"I'm a great dev!"} />
+                <TweetsButtonsBar />
+                {/*{tweets}*/}
+                <Tweet/>
+                <Tweet/>
+                <Tweet/>
+                <Tweet/>
+                <Tweet/>
+            </div>
+        );
+    }
+}
+
+class ButtonsBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="ButtonsBar">
+                <img className={"ProfilePicture"} src={this.props.profilePicturePath}/>
+                <div className={"ButtonsBarButtonContainer"}>
+                    <Button className={"FollowingFollowersButtons"} shape={"round"}>
+                        Followings
+                    </Button>
+                    <Button className={"FollowingFollowersButtons"} shape={"round"}>
+                        Followers
+                    </Button>
+                    <Button className={"FollowUnfollowButton"} shape={"round"}>
+                        {this.props.isFollowed ?
+                            <span>
+                            Unfollow
+                        </span>
+                            :
+                            <span>
+                            Follow
+                        </span>
+                        }
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+class UserInformationPanel extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="UserInformationPanel">
+                <div className={"UsersName"}>
+                    {this.props.UsersName}
+                </div>
+                <div className={"Username"}>
+                    @{this.props.Username}
+                </div>
+                <div className={"Bio"}>
+                    {this.props.Bio}
+                </div>
+            </div>
+        );
+    }
+}
+
+class TweetsButtonsBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="TweetsButtonsBar">
+                <Button className={"TweetsRetweetsButton"}>
+                    Tweets
+                </Button>
+                <Button className={"TweetsRetweetsButton"}>
+                    Retweets
+                </Button>
+            </div>
+        );
+    }
+}
